1e07eaf9d9aeafe676d114d828d3b661b9bab792
Junior <jose.junior@exactworks.com.br>
2020-11-22 18:49:30 -0300

    blocos de hobbies e habilidades

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 		resources :profiles, only: [:index, :update]
 		resources :experiences, only: [:index, :create, :destroy]
 		resources :educations, only: [:index, :create, :destroy]
+		resources :skills, only: [:index, :create, :destroy]
+		resources :hobbies, only: [:index, :create, :destroy]
 	end
 	
 	resources :tags, only: :index
